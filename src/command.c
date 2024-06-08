839704409cd9ff93cfecba58bccf5a5a06f725cd
Alug <alufoliehorst@gmail.com>
2024-06-08 14:24:56 +0200

    "fix" wait on file exec
    why just why tf

--- src/command.c
+++ src/command.c
@@ -856,7 +856,8 @@ static void COM_Exec_f(void)
 		CONS_Printf(M_GetText("executing %s\n"), COM_Argv(1));
 
 	// insert text file into the command buffer
-	COM_ImmedExecute((char *)buf);
+	COM_BufAddTextEx((char *)buf, com_flags);
+	COM_BufAddTextEx("\n", com_flags);
 
 	// free buffer
 	Z_Free(buf);
