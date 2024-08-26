692a4f453960f9b570317ff092678558d33c8600
hayaunderscore <stophayapls123@proton.me>
2024-08-26 09:59:39 +0800

    Reapply -file fix, with no crashes this time

--- src/d_main.cpp
+++ src/d_main.cpp
@@ -1832,8 +1832,6 @@ void D_SRB2Main(void)
 
 	CON_Init();
 
-	CON_SetLoadingProgress(LOADED_HUINIT);
-
 	D_RegisterServerCommands();
 	D_RegisterClientCommands(); // be sure that this is called before D_CheckNetGame
 	R_RegisterEngineStuff();
@@ -1841,7 +1839,12 @@ void D_SRB2Main(void)
 
 	I_RegisterSysCommands();
 	
+	CON_SetLoadingProgress(LOADED_HUINIT);
+	
 	CONS_Printf("W_InitMultipleFiles(): Adding external PWADs.\n");
+	
+	// HACK: Refer to https://git.do.srb2.org/KartKrew/RingRacers/-/merge_requests/29#note_61574
+	partadd_earliestfile = numwadfiles;
 	W_InitMultipleFiles(startuppwads, true);
 
 	// Only search for pwad maps and reload graphics if we actually have a pwad added
@@ -1851,10 +1854,11 @@ void D_SRB2Main(void)
 		// search for pwad maps
 		//
 		P_InitMapData();
-		// HU_LoadGraphics();
+		HU_LoadGraphics();
 	}
 	
 	D_CleanFile(startuppwads);
+	partadd_earliestfile = UINT16_MAX
 
 	CON_SetLoadingProgress(LOADED_PWAD);
 
